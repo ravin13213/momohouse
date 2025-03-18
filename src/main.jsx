ea6f126f25d04e36bb11e817b75556b32734880d
ravin13213 <rabindranaga111@gmail.com>
2025-03-18 14:04:15 +0545

    added

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './Context/CartProvider.jsx'
 import { Auth0Provider } from "@auth0/auth0-react";
 
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <CartProvider>
       <Auth0Provider
         domain="dev-jazyx0sa8sa7g4oe.us.auth0.com"
         clientId="1NDXSzqbt9GC2rnuPa2KYIUYDFK8LtAM"
@@ -19,6 +17,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </Auth0Provider>
       
-    </CartProvider>
   </BrowserRouter>
 );
